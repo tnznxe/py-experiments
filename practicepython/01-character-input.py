c1370bdbf6a9e969885903157ad6a465f5569bbf
Tenzin <tenzinxe@gmail.com>
2020-03-16 09:38:11 +0545

    Add question 1:python practice

---
+++ practicepython/01-character-input.py
@@ -0,0 +1,7 @@
+# first work
+# https: // www.practicepython.org/exercise/2014/01/29/01-character-input.html
+name = input('enter your name; ')
+age = input('enter your age; ')
+age = int(age)
+a = 100 - age
+print(f'hi {name}, you will reach 100 year in {a} years ')
